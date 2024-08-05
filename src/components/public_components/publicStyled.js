49bf69805fc99ea4a2ba00d35c9c43f6c458d39b
bluetree7878 <bluetree7878@naver.com>
2024-08-06 03:33:36 +0900

    publish/푸터 컴포넌트 퍼블리싱 및 링크 기능#45

--- src/components/public_components/publicStyled.js
+++ src/components/public_components/publicStyled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const HeaderDiv = styled.div`
   display: flex;
@@ -11,13 +12,13 @@ export const HeaderDiv = styled.div`
   padding-top: 15px;
   z-index: 10;
 `;
-//
+
 export const HeaderImg = styled.img`
   width: 125px;
   height: 75px;
   cursor: pointer;
 `;
-export const LoginSerchDiv = styled.div`
+export const LoginSearchDiv = styled.div`
   font-family: 'countach', sans-serif;
   display: flex;
   align-items: center;
@@ -31,6 +32,93 @@ export const LoginSerchDiv = styled.div`
 export const HeaderTextDiv = styled.div`
   color: #f7f9f3;
 `;
-export const HeaderCoulumnDiv = styled.div`
+export const HeaderColumnDiv = styled.div`
   display: flex;
+`;
+
+export const FooterDiv = styled.div`
+  font-family: 'Pretendard-Regular';
+
+  margin-top: 80px;
+  display: flex;
+  color: #f7f9f3;
+  flex-direction: column;
+`;
+
+export const SectionWrapperDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const HorizontalLine = styled.hr`
+  width: 100%;
+  border: none;
+  border-top: 2px solid #f7f9f3;
+  margin-top: 15px;
+`;
+
+export const SectionDiv = styled.div`
+  padding: 30px 50px 30px 50px;
+  width: 50%;
+  font-size: 18px;
+  margin: 20px;
+`;
+
+export const JoinGroupDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const JoinImg = styled.img`
+  height: 20px;
+  width: 20px;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
+
+export const HrTopDiv = styled.div`
+  font-weight: 600;
+`;
+
+export const DescriptionDiv = styled.div`
+  margin-top: 20px;
+`;
+
+export const MoreModernDiv = styled.div`
+  margin-top: 50px;
+`;
+
+export const ModernAgileYoutubeDiv = styled.div`
+  cursor: pointer;
+  display: inline-block;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const CopywriterWrapperDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin: 15px;
+`;
+
+export const CopywriterDiv = styled.div`
+  font-size: 15px;
+`;
+
+export const MenuDiv = styled.div`
+  font-weight: 500;
+`;
+
+export const MenuLink = styled(Link)`
+  margin: 0 30px;
+  color: #8d8d8d;
+  &:hover {
+    text-decoration: underline;
+  }
 `;
--- src/pages/intro/IntroPage.jsx
+++ src/pages/intro/IntroPage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import * as S from './IntroPageStyled';
 import Header from '../../components/public_components/Header';
 import MovieDetail from '../../components/page_components/intro/MovieDetail';
+import Footer from '../../components/public_components/Footer';
 
 export default function IntroPage() {
   return (
     <>
       <Header></Header>
       <MovieDetail></MovieDetail>
+      <Footer></Footer>
     </>
   );
 }
