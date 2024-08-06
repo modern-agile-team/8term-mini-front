3a83a726c158c96f3376acea6c0931c945256562
bluetree7878 <bluetree7878@naver.com>
2024-08-06 16:43:33 +0900

    feat/프로필 수정버튼 누를 시만 수정 가능/#54

--- src/components/page_components/mypage/UserProfile.jsx
+++ src/components/page_components/mypage/UserProfile.jsx
@@ -1,6 +1,18 @@
+import React, { useState } from 'react';
 import * as S from './MyPageStyled';
 import profileimg from '/profileimg.png';
+
 export default function UserProfile() {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleCompleteClick = () => {
+    setIsEditing(false);
+  };
+
   return (
     <>
       <S.UserProfileDiv>
@@ -13,30 +25,36 @@ export default function UserProfile() {
           <S.UserProfileColumnDiv $marginBottom={'70px'} $marginTop={'20px'}>
             <S.UserProfileRowDiv>
               <S.labelDiv>닉네임</S.labelDiv>
-              <S.UserInfoInput></S.UserInfoInput>
+              <S.UserInfoInput disabled={!isEditing}></S.UserInfoInput>
             </S.UserProfileRowDiv>
             <S.UserProfileRowDiv>
               <S.labelDiv>아이디</S.labelDiv>
-              <S.UserInfoInput></S.UserInfoInput>
+              <S.UserInfoInput disabled={!isEditing}></S.UserInfoInput>
             </S.UserProfileRowDiv>
           </S.UserProfileColumnDiv>
 
           <S.UserProfileColumnDiv $marginBottom={'70px'}>
             <S.UserProfileRowDiv>
               <S.labelDiv>비밀번호 수정</S.labelDiv>
-              <S.PasswordInput></S.PasswordInput>
+              <S.PasswordInput disabled={!isEditing}></S.PasswordInput>
             </S.UserProfileRowDiv>
             <S.UserProfileRowDiv>
               <S.labelDiv>비밀번호 확인</S.labelDiv>
-              <S.PasswordInput></S.PasswordInput>
+              <S.PasswordInput disabled={!isEditing}></S.PasswordInput>
             </S.UserProfileRowDiv>
           </S.UserProfileColumnDiv>
 
           <S.ButtonDiv>
-            <S.Button $bgColor="#F7F9F3" $marginRight="30px">
-              수정
-            </S.Button>
-            <S.Button $fontColor="#F7F9F3">완료</S.Button>
+            {!isEditing && (
+              <S.Button $bgColor="#F7F9F3" onClick={handleEditClick}>
+                수정
+              </S.Button>
+            )}
+            {isEditing && (
+              <S.Button $fontColor="#F7F9F3" onClick={handleCompleteClick}>
+                완료
+              </S.Button>
+            )}
           </S.ButtonDiv>
         </S.UserProfileInfoDiv>
       </S.UserProfileDiv>
