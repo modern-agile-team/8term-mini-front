c502e795092532f24b73413504386b22aed2cc50
bluetree7878 <bluetree7878@naver.com>
2024-08-19 11:23:10 +0900

    feat/로그아웃 기능 구현/#74

--- src/components/public_components/Header.jsx
+++ src/components/public_components/Header.jsx
@@ -1,9 +1,51 @@
 import { Link } from 'react-router-dom';
 import * as S from './publicStyled';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 /** @헤더 헤더 컴포넌트 */
 export default function Header() {
   const baseUrl = import.meta.env.VITE_IMG_BASE_URL;
+  const [isLogin, setIsLogin] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const user = localStorage.getItem('user');
+
+    if (token && user) {
+      setIsLogin(true);
+    } else {
+      setIsLogin(false);
+    }
+  }, []);
+
+  function handleLogout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    setIsLogin(false);
+    navigate('/');
+  }
+
+  if (isLogin) {
+    return (
+      <>
+        <S.HeaderDiv>
+          <Link to="/">
+            <S.HeaderImg src={`${baseUrl}logo3.png`}></S.HeaderImg>
+          </Link>
+          <S.LoginSearchDiv>
+            <S.HeaderColumnDiv>
+              <S.HeaderTextDiv onClick={handleLogout}>LOGOUT</S.HeaderTextDiv>
+            </S.HeaderColumnDiv>
+            <Link to="/mypage">
+              <S.HeaderTextDiv>MYPAGE</S.HeaderTextDiv>
+            </Link>
+          </S.LoginSearchDiv>
+        </S.HeaderDiv>
+      </>
+    );
+  }
 
   return (
     <>
