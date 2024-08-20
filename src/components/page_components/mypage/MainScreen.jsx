3be4ba571cf932828e37086450e66faa79ae37fe
bluetree7878 <bluetree7878@naver.com>
2024-08-20 14:36:36 +0900

    feat/마이페이지 로그인 안 할 시 로그인페이지 이동/#80

--- src/components/page_components/mypage/MainScreen.jsx
+++ src/components/page_components/mypage/MainScreen.jsx
@@ -1,6 +1,16 @@
 import * as S from './MyPageStyled';
 import UserProfile from './UserProfile';
+import { Navigate } from 'react-router-dom';
+
 export default function MainScreen() {
+  const token = localStorage.getItem('token');
+  const user = localStorage.getItem('user');
+
+  if (!token || !user) {
+    console.error('Error: 사용자 정보를 읽을 수 없습니다.');
+    return <Navigate to="/login" replace />;
+  }
+
   return (
     <>
       <S.MainScreenDiv>
