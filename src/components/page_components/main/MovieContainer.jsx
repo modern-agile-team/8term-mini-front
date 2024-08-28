794c9ead37558dc15fc1be7ee3909ff7049b9657
GuDoYoon <154503723+rhehfl@users.noreply.github.com>
2024-08-28 12:53:28 +0900

    Update MovieContainer.jsx

--- src/components/page_components/main/MovieContainer.jsx
+++ src/components/page_components/main/MovieContainer.jsx
@@ -6,17 +6,7 @@ import SearchBar from './SearchBar.jsx';
 import getUserInfo from '../../../function/getUserInfo.js';
 import MovieSortBar from './MovieSortBar.jsx';
 export default function MovieContainer() {
-  //임시 로그인
-  localStorage.setItem('token', 'sadasdjkfhsadkjfhasieulf');
-  localStorage.setItem(
-    'user',
-    JSON.stringify({
-      user_id: 1,
-      id: 'dg1418',
-      nickName: '관리자',
-      profile: 'profileimg1.png',
-    })
-  );
+ 
   const [userId] = getUserInfo();
   const [movies, setMovies] = useState([]);
   const [wishList, setWishList] = useState();
