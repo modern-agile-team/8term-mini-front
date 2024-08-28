4d8a3964d1b3182b1475f7d37a608bc3965c48bd
GuDoYoon <rhehfl418q@gmail.com>
2024-08-28 18:15:11 +0900

    server

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '8term-mini-front',
   server: {
     port: 3000,
   },
