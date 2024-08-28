a5ac7fe1a487ba6702ff1f4fa200e7a083d1ca51
bluetree7878 <bluetree7878@naver.com>
2024-08-28 11:54:24 +0900

    docs/meta태그 추가 및 vite파일 수정/#102

--- src/axios/instance.js
+++ src/axios/instance.js
@@ -7,13 +7,13 @@ import {
 } from './function';
 //베이스  instance
 const basicAxios = axios.create({
-  baseURL: `/api/`,
+  baseURL: import.meta.env.VITE_BACK_BASE_URL,
   withCredentials: true,
   timeout: 3000,
 });
 //인증용 instance
 const authAxios = axios.create({
-  baseURL: `/api/`,
+  baseURL: import.meta.env.VITE_BACK_BASE_URL,
   withCredentials: true,
   timeout: 3000,
 });
